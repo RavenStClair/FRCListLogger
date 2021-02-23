d84dfad03c1ccdf76844fbaaadbc1c9d14b200b7
RavenStClair <59072269+RavenStClair@users.noreply.github.com>
2021-02-23 05:41:56 -0500

    Update main.java

--- src/app/main.java
+++ src/app/main.java
@@ -2,13 +2,8 @@ package app;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        PathInterpolator.setFile("C:/Users/Raven/Documents/test.csv");
-        System.out.print("raw_r = ");
-        /*for (float i =0;i<12;i= (float) (i + 0.01)) {
-            System.out.print(calcPositions(i)[0]+", ");
-        }
-        System.out.print("raw_l = ");
-*/
+        PathInterpolator.setFile("FULL_FILE_DIRECTORY");
+        
     }
 
 }
